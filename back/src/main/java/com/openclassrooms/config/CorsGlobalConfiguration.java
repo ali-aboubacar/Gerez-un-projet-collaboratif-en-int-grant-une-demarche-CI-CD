09371d79db1ebdc52704aeea18caaa0bd924f7c2
ali-aboubacar <ali.02.aboubacar@gmail>
2025-09-18 17:24:43 +0200

    deploy to server ........

---
+++ back/src/main/java/com/openclassrooms/config/CorsGlobalConfiguration.java
@@ -0,0 +1,26 @@
+package com.openclassrooms.bobapp.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://178.62.54.206:8089");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
